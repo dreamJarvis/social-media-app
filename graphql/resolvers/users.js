26722b1c67bb5c2e0f7c8bfe91ee299bbe24306f
dreamJarvis <jhatarun16@gmail.com>
2021-05-29 19:40:47 +0530

    changes added

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,46 +2,98 @@ const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
 // lib's for hashing & crypting passwords
+const { validateRegisterInput, validateLoginInput } = require('../../utils/validators');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+
+// helper funct's
+function generateToken(user){
+	return jwt.sign(
+		{
+			id: user.id, 
+			email: user.email, 
+			username: user.username
+		}, 
+		SECRET_KEY, 
+		{ expiresIn: '1h'}  
+	);
+}
 
 module.exports = {
-    Mutation: {
-        async register(
-            _, 
-            {   
-                registerInput: { username, email, password, confirmPassword}
-            }, 
-            context, 
-            info
-        ){
-            // TODO: Vaidate user data
-            
-            // TODO: Make sure user doesn't already exists
+	Mutation: {	
+		//** login inputs validation
+		async login(_, {username, password}){
+			const { errors, valid } = validateLoginInput(username, password);
+			
+			if(!valid){
+				throw new UserInputError('Wrong Dredentials', { errors });
+			}
+	
+			const user = await User.findOne({ username });
+			if(!user){
+				errors.general = 'User not found !';
+				throw new UserInputError('User not found !', { errors });
+			}
+	
+			const match = await bcrypt.compare(password, user.password);
+			if(!match){
+				errors.general = 'Wrong Dredentials';
+				throw new UserInputError('Wrong Dredentials', { errors });
+			}
+	
+			const token = generateToken(user);
+			
+			return {
+				...user._doc,
+				id: user._id,
+				token
+			};
+		},
 
-            // TODO: hash password and create auth token 
-            password = await bcrypt.hash(password, 12);
+		//** register input validation
+		async register(
+			_, 
+			{   
+				registerInput: { username, email, password, confirmPassword}
+			}
+		){
+			// Vaidate user data
+			const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
 
-            const newUser = new User({
-                username,
-                password,
-                email, 
-                createdAt: new Date().toISOString()
-            });
+			if(!valid) {
+				throw new UserInputError('Errors', { errors });
+			}
+			
+			// Make sure user doesn't already exists
+			const user = await User.findOne({ username });
+			if(user){
+				throw new UserInputError('User is taken', {
+					error: {
+						username: 'This user is taken'
+					}
+				});
+			}
 
-            const res = await newUser.save();   // saving the new data to the db
+			// hash password and create auth token 
+			password = await bcrypt.hash(password, 12);
 
-            const token = jwt.sign({
-                id: res.id, 
-                email: res.email, 
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h'}  );
-            
-            return {
-                ...res._doc,
-                id: res._id,
-                token
-            };
-        }
-    }
+			const newUser = new User({
+				username,
+				password,
+				email, 
+				createdAt: new Date().toISOString()
+			});
+
+			const res = await newUser.save();   // saving the new data to the db
+
+			const token = generateToken(res);
+			
+			return {
+				...res._doc,
+				id: res._id,
+				token
+			};
+		}
+	}
 }
--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const {ApolloServer} = require('apollo-server');
+const {ApolloServer, PubSub} = require('apollo-server');
 const mongoose = require('mongoose');
 
 const { MONGODB } = require('./config.js');
 const typeDefs = require('./graphql/typeDefs.js');
 const resolvers = require('./graphql/resolvers/index.js');
 
+const pubsub = new PubSub(); //** for subscriptions
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })   //TODO: what does this does ??
 });
 
 mongoose.connect(   
-    MONGODB, 
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    })
-    .then(() => {
-        console.log('connected to mongodb...');
-        return server.listen({port: 5000})
-    })
-    .then((res) => {
-        console.log(`server running at ${res.url}`);
-    });
+  MONGODB, 
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('connected to mongodb...');
+    return server.listen({port: 5000})
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  });
