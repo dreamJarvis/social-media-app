6e1b4bd60c63b230774e322901a310e0adadcb53
dreamJarvis <jhatarun16@gmail.com>
2021-05-30 21:58:22 +0530

    added posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container, Header } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react';
+
+import PostCard from '../components/PostCard';
 
 function Home(){
+  // const { loading, data: {getPosts: posts} } = useQuery(FETCH_POSTS_QUERY);        //TODO: why is this not working ??
+  const { loading, data} = useQuery(FETCH_POSTS_QUERY);
+
+  // if(data){
+  //   console.log(data.getPosts);
+  // }
+
+  // TODO: Not working ????
+  // if(posts){
+  //   console.log(posts);
+  // }
+    
   return(
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid columns={3} divided>
+    <Grid.Row>
+      <h1>Recent Posts</h1>
+    </Grid.Row>
+    <Grid.Row>
+      {loading ? (
+        <h1>loading posts ...</h1>
+      ):(
+        data && data.getPosts.map((post, index) => 
+          <Grid.Column key={post.id} style={{marginBottom: 20}}>
+            <PostCard post={post}/>
+          </Grid.Column>
+        ) 
+      )}  
+    </Grid.Row>
+    </Grid>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts{
+      id
+      body
+      createdAt 
+      username
+      likeCount
+      likes{
+        username
+      }
+      commentCount
+      comments{
+        id 
+        username 
+        createdAt 
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
